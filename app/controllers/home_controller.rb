a1e6b6d15e2c5a8df7d76b04c5122454f76bc1f1
Henry Gabriel Acosta <gabriel_23hgap@hotmail.com>
2016-12-10 18:53:33 +0000

    testing data gem

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
     
     def index
-        @data = { 'name': 'henry', 'age': '24' }
+        @data = { 'name': 'henry', 'test': 'this is, just testing data past through from controller to script' }
         gon.data = @data
         
     end
